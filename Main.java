3dc9aff4891c68c39fae3ac0a0711895ba20f8eb
mcgml <mcgml@cardiff.ac.uk>
2015-10-27 13:44:10 +0000

    updates

--- Main.java
+++ Main.java
@@ -12,56 +12,61 @@ import java.util.logging.Logger;
 public class Main {
 
     private static final Logger log = Logger.getLogger(Main.class.getName());
-    private static final double version = 0.2;
+    private static final double version = 0.3;
+    private static boolean newDatabase = false;
 
     public static void main(String[] args) throws InvalidPropertiesFormatException {
 
-        if (args.length != 4) {
-            System.err.println("Usage: <GenotypeVCFFile> <AnnotatedVCFFile> <Neo4jDBPath> <NewDB>");
+        if (args.length < 2 || args.length > 3) {
+            System.err.println("Usage: <VCF> <db>");
+            System.err.println("Options: -n new database");
             System.exit(1);
         }
 
         log.log(Level.INFO, "ImportToNeo4j v" + version);
 
         //update or overwrite
-        boolean overwriteDB = Boolean.parseBoolean(args[3]);
+        for (String arg : args){
+            if (args.equals("-n")){
+                newDatabase = true;
+            }
+        }
 
-        //delete existing DB
-        if (overwriteDB) {
-            log.log(Level.INFO, "Deleting current DB");
+        log.log(Level.INFO, "New database: " + newDatabase);
+
+        if (newDatabase) {
+            log.log(Level.INFO, "Deleting existing database");
             try{
-                FileUtils.deleteRecursively(new File(args[2]));
+                FileUtils.deleteRecursively(new File(args[1]));
             } catch (IOException e){
                 log.log(Level.SEVERE, "Could not delete database: " + e.getMessage());
                 System.exit(1);
             }
         }
 
-        log.log(Level.INFO, "Importing " + args[0] + " to " + args[2]);
+        log.log(Level.INFO, "Importing " + args[0] + " to " + args[1]);
 
         //create VCF file parser
         VCFFileReader variantVcfFileReader = new VCFFileReader(new File(args[0]), new File(args[0] + ".idx"));
-        VCFFileReader annotationVcfFileReader = new VCFFileReader(new File(args[1]), new File(args[1] + ".idx"));
 
         //create database object
-        VariantDatabase variantDatabase = new VariantDatabase(variantVcfFileReader, annotationVcfFileReader, new File(args[2]));
+        VariantDatabase variantDatabase = new VariantDatabase(variantVcfFileReader, new File(args[1]));
 
         //create new DB
         variantDatabase.startDatabase();
-        if (overwriteDB) variantDatabase.createIndexes();
+        if (newDatabase) variantDatabase.createIndexes();
 
-        variantDatabase.loadVCFFiles();
-        if (overwriteDB) variantDatabase.addUsers();
+        variantDatabase.loadVCF();
+        if (newDatabase) variantDatabase.addUsers();
 
         variantDatabase.addSampleAndRunInfoNodes();
         variantDatabase.addVariantNodesAndGenotypeRelationships();
         variantDatabase.addAnnotations();
-        if (overwriteDB) variantDatabase.addGenePanels();
+        if (newDatabase) variantDatabase.addGenePanels();
 
         variantDatabase.shutdownDatabase();
 
         variantVcfFileReader.close();
-        annotationVcfFileReader.close();
 
     }
 
